82bccda328062faa3ae2918a85e4a67244210f40
Konstantinos Lamprakis <klamprak@2-H-9.42heilbronn.de>
2024-03-05 11:34:49 +0100

    Implement ft_memcpy

--- libft.h
+++ libft.h
@@ -6,9 +6,13 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 09:50:57 by klamprak          #+#    #+#             */
-/*   Updated: 2024/03/05 10:59:51 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/03/05 11:22:17 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+	<stddef.h>: size_t
+ */
 
 #ifndef LIBFT_H
 # define LIBFT_H
@@ -22,4 +26,6 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*memcpy(void *restrict dst, const void *restrict src, size_t n);
 #endif
