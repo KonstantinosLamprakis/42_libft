f4270f22ce114c307775a2cfe38ce367bb028d5f
Konstantinos Lamprakis <klamprak@2-F-5.42heilbronn.de>
2024-03-06 17:39:56 +0100

    Implement ft_calloc and fix a typo on ft_strdup

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 17:11:28 by klamprak          #+#    #+#             */
+/*   Updated: 2024/03/06 17:36:43 by klamprak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*calloc(size_t count, size_t size)
+{
+	void	*result;
+
+	result = malloc(count * size);
+	if (!result)
+		return (NULL);
+	ft_bzero(result, count * size);
+	return (result);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 09:50:57 by klamprak          #+#    #+#             */
-/*   Updated: 2024/03/06 17:07:12 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/03/06 17:39:28 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*calloc(size_t count, size_t size);
+char	*ft_strdup(const char	*s1);
 #endif
