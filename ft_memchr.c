ccdb23f18f144e34852ec01338b343353bdf4d35
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-03-06 13:09:32 +0100

    Implement ft_memcmp

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 12:32:45 by klamprak          #+#    #+#             */
-/*   Updated: 2024/03/06 12:44:10 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/03/06 13:09:02 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	const unsigned char *temp = (unsigned char *) s;
+	size_t				i;
+	const unsigned char	*temp;
 
+	temp = (unsigned char *) s;
 	i = 0;
 	while (i < n)
 	{
