5c26b1862ef42d97678a74ebb5f858f99cebcaa4
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-03-08 15:34:55 +0100

    Fix overflow problem in substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 17:41:33 by klamprak          #+#    #+#             */
-/*   Updated: 2024/03/07 12:14:13 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/03/08 15:31:01 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*result;
 
+	if ((int) len < 0)
+		len = ft_strlen(s);
 	if (start > ft_strlen(s) || len < 0)
 	{
 		result = malloc(1 * sizeof(char));
@@ -41,7 +43,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 // #include <stdio.h>
 // int	main(void)
 // {
-// 	char const *s = "This is a test";
-// 	char *result = ft_substr(s, 5, 1);
+// 	// char const *s = "This is a test";
+// 	// char *result = ft_substr(s, 5, 1);
+// 	// char *result = ft_substr("hola", 0, 42949672951);
+// 	char *result = ft_substr("hola", 0, 18446744073709551615);
 // 	puts(result);
 // }
