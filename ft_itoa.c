99f247f4b8e94fc11911ccb385d8b6d36153e1fa
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-03-08 16:24:07 +0100

    Simplify ft_itoa and reduce mem size that allocates

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,62 +6,65 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 09:10:42 by klamprak          #+#    #+#             */
-/*   Updated: 2024/03/07 12:38:58 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/03/08 16:22:58 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	positive_itoa(int n, char *result, int i);
+static void	positive_itoa(long n, char **result, int i, int sign);
 
 char	*ft_itoa(int n)
 {
 	char	*result;
 	int		i;
+	long	l_num;
+	int		sign;
 
-	result = malloc(12 * sizeof(char));
-	if (!result)
-		return (NULL);
+	l_num = n;
+	sign = 0;
+	result = NULL;
 	i = 0;
-	if (n == -2147483648)
+	if (l_num < 0)
 	{
-		result[0] = '-';
-		result[1] = '2';
-		i += 2;
-		n = 147483648;
+		l_num = -l_num;
+		sign = 1;
 	}
-	if (n < 0)
-	{
-		result[i++] = '-';
-		n = -n;
-	}
-	positive_itoa(n, result, i);
+	positive_itoa(l_num, &result, i, sign);
 	return (result);
 }
 
-static void	positive_itoa(int n, char *result, int i)
+static void	positive_itoa(long n, char **result, int i, int sign)
 {
-	int		div;
-	int		temp_num;
+	long	div;
+	long	temp_num;
+	int		digits;
 
 	temp_num = n;
 	div = 1;
+	digits = 1;
 	while (temp_num / 10 != 0)
 	{
 		temp_num /= 10;
 		div *= 10;
+		digits++;
 	}
+	*result = malloc((digits + 1 + sign) * sizeof(char));
+	if (!(*result))
+		return ;
+	if (sign == 1)
+		(*result)[i++] = '-';
 	while (div >= 1)
 	{
-		result[i++] = '0' + (n / div);
+		(*result)[i++] = '0' + (n / div);
 		n %= div;
 		div /= 10;
 	}
-	result[i++] = '\0';
+	(*result)[i++] = '\0';
 }
 
 // #include <stdio.h>
 // int	main(void)
 // {
-// 	puts(ft_itoa(-2147483648));
+// 	puts(ft_itoa(-124));
 // }
