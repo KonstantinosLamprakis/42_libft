e66ec36d4ebf3696988f682f7bb7f2b5d859a9f0
Konstantinos Lamprakis <klamprak@3-F-14.42heilbronn.de>
2024-03-08 11:02:55 +0100

    Implement bonus: lstdelone

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 09:50:57 by klamprak          #+#    #+#             */
-/*   Updated: 2024/03/08 10:36:11 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/03/08 10:56:31 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,4 +76,5 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 #endif
